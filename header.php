fed408ba51a127c8107a93402b4ff6e71cbac075
William <williamgr.dev@gmail.com>
2023-09-11 21:59:37 -0300

    Corrigindo caminho

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 		<header>
 			<nav>
 				<ul>
-					<li class="current_page_item"><a href="/testewp/">Menu</a></li>
+					<li class="current_page_item"><a href="/">Menu</a></li>
 					<li><a href="/testewp/sobre">Sobre</a></li>
 					<li><a href="/testewp/contato">Contato</a></li>
 				</ul>
