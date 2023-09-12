473e59c084e5d9696c6e49c39752403ef5fab28a
William <williamgr.dev@gmail.com>
2023-09-11 21:55:06 -0300

    Trazendo os títulos das páginas com funções de worpress

--- page-home.php
+++ page-home.php
@@ -1,10 +1,11 @@
 <?php
-//Template Name: Menu da Semana
+// Template Name: Menu da Semana
 ?>
-
 <?php get_header(); ?>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<section class="container">
-			<h2 class="subtitulo">Menu da Semana</h2>
+			<h2 class="subtitulo"><?php the_title(); ?></h2>
 
 			<div class="menu-item grid-8">
 				<h2>Peixes</h2>
@@ -59,5 +60,7 @@
 					</li>
 				</ul>
 			</div>
+
 		</section>
+<?php endwhile; else: endif; ?>
 <?php get_footer(); ?>
