be65e10b2f295eed06b77e12ac69038a478c36dc
William <williamgr.dev@gmail.com>
2023-09-15 16:17:26 -0300

    Incluindo o arquivo taxonomy

--- functions.php
+++ functions.php
@@ -2,3 +2,4 @@
 
 require_once get_template_directory() . '/core/cpt.php';
 require_once get_template_directory() . '/core/metabox.php';
+require_once get_template_directory() . '/core/taxonomy.php';
